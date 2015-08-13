4387127fee89ff46055df20d2e969d0438294d7d
andyjansson <andyjansson@users.noreply.github.com>
2015-08-13 11:25:04 +0200

    Add test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,19 @@
-/*
 var path = require('path');
 var fs = require('fs');
 var svg = require('../');
+var assert = require('assert');
+var expected = require('./expected.json');
 
-fs.readFile(path.join(__dirname, 'pathFont.svg'), function (err, contents) {
-	svg(contents).then(function(results) {
-		console.log(results);
+describe('svg-font-parser', function () {
+	it('can parse .svg fonts', function (done) {
+		fs.readFile(path.join(__dirname, 'pathFont.svg'), function (err, contents) {
+			if (err) throw err;
+			svg(contents).then(function (results) {
+				assert.deepEqual(results, expected);
+				done();
+			}, function () {
+				assert.fail("Parsing font failed");
+			});
+		});
 	});
-});*/
+});
